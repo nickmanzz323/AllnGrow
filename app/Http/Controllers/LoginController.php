a8d1ba5167317536246b223e935ded93c1ca885b
Christian <christiantjandra33@gmail.com>
2025-10-12 18:59:49 +0700

    fix .env

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function postLogin(Request $request)
     {
-        if(Auth::attempt($request->only('email', 'password'))){
-            return redirect('/studentDashboard');
-        }
+        $credentials = $request->only('email', 'password');
 
-        return redirect('/login');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->level === 'teacher') {
+                return redirect('/teacherDashboard');
+            } elseif ($user->level === 'student') {
+                return redirect('/studentDashboard');
+            } else {
+                Auth::logout();
+                return redirect('/login')->withErrors(['level' => 'Invalid user level.']);
+            }
+        } else {
+            return redirect('/login')->withErrors(['email' => 'Invalid credentials.']);
+        }
     }
 }
